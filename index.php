97cae1da162e34089f9447818886a951c61e2923
Joluq <92047089+joluquispe@users.noreply.github.com>
2023-07-25 21:41:41 -0500

    d

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
     echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";
+    
 
 ?>
