87ffcad898bf5d6b211a85c1f961c1cea40f0d7b
Joluq <92047089+joluquispe@users.noreply.github.com>
2023-07-27 20:20:27 -0500

    nuevas modificaciones en index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    echo "Esto es la creacion del archivo index del proyecto";    echo "Esto es la creacion del archivo index del proyecto";
+    echo "Esto es la creacion del archivo index del proyecto";    
     echo "Esto es la creacion del archivo index del proyecto";
    
 ?>
