db35e8050eb15b1c38a214d7e88de18f7d3ff390
Joluq <92047089+joluquispe@users.noreply.github.com>
2023-08-06 12:18:10 -0500

    añadiendo cambion iniciales

--- index.php
+++ index.php
@@ -3,4 +3,13 @@
     echo "Esto es la creacion del archivo index del proyecto";    
     echo "Esto es la creacion del archivo index del proyecto";
    
+    
+    echo "Esto es la creacion del archivo index del proyecto";    
+    echo "Esto es la creacion del archivo index del proyecto";
+    
+    echo "Esto es la creacion del archivo index del proyecto";    
+    echo "Esto es la creacion del archivo index del proyecto";
+    
+    echo "Esto es la creacion del archivo index del proyecto";    
+    echo "Esto es la creacion del archivo index del proyecto";
 ?>
