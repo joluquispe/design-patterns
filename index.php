f427c682c9361c70566381be625919fd4366f556
joluquispe <joluquispe2@gmail.com>
2023-08-30 19:04:44 -0500

    modificaciones de index

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
     </head>
     <body>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo minus debitis accusamus eligendi doloribus voluptatum ipsa ex dolorum blanditiis natus, vero et quo corporis ea autem quae saepe? Aspernatur, consequatur!</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis ipsam nesciunt non velit iure, odit porro deleniti dicta culpa saepe quo id, nulla quos eaque voluptatem facilis pariatur quod necessitatibus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat excepturi amet ea et distinctio impedit non modi odit quibusdam harum temporibus vel quae ipsum autem quam, eos dignissimos vero? Cupiditate! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi sed repudiandae beatae tempore minus aliquam! Aspernatur est, nulla tenetur maiores facilis similique nesciunt illum consectetur qui, nihil inventore, odio ipsum.</p>
     </body>
     </html>
 
